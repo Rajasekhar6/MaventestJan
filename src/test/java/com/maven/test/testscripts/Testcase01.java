dc410f86aab26a6eb248f917babeb435af243da9
RajaSekhar.Kakumanu <>
2018-02-16 20:31:10 +0530

    Modified change

--- src/test/java/com/maven/test/testscripts/Testcase01.java
+++ src/test/java/com/maven/test/testscripts/Testcase01.java
@@ -11,42 +11,41 @@ import org.testng.annotations.Test;
 import com.maven.test.Helper.Basepage;
 import com.maven.test.pages.Login;
 
-public class Testcase01 extends Basepage {
+public class Testcase01 extends Basepage{
 
-	// Declared in Basepage
-	// adding new lines
 	@Test
 	public void Logintest()
 
 	{
+	//	test = rep.startTest("Logintest");
+	//	test.log(LogStatus.INFO	, "loginwith userid and password");
 		try {
-			Login lg = PageFactory.initElements(Basepage.driver, Login.class);
-			lg.textboxusername().sendKeys("rajasekhar.kakumanu");
-			lg.textboxpassword().sendKeys("India2018*");
-			lg.buttonlogin().click();
-		} catch (Exception e) {
+		Login lg = PageFactory.initElements(Basepage.driver,Login.class);
+		lg.textboxusername().sendKeys("rajasekhar.kakumanu");
+		lg.textboxpassword().sendKeys("India2018*");
+		lg.buttonlogin().click();
+		}catch(Exception e)
+		{
 			e.printStackTrace();
 		}
+		
+	String text = Basepage.driver.getTitle();
+	System.out.println(text);
+	if(text.contains("Trinet - Homeeee"))
+	//	test.log(LogStatus.INFO, "validating the page title");
+	{
+	//	test.log(LogStatus.FAIL, "title not matched");
+	//	Assert.fail("Checking screen shot scenario");
+		System.out.println("Login is success");
+	}
+	//else {
+	System.out.println("Login is Fail");
+	//Assert.fail("Login is unsucessful");
+	}
 
-		String text = Basepage.driver.getTitle();
-		System.out.println("Title of the page is  : " + text);
-		if (text.contains("Trinet - Home"))
-
-		{
-	//		test.log(LogStatus.INFO, "validating the page title");
-			// test.log(LogStatus.FAIL, "title not matched");
-			// Assert.fail("Checking screen shot scenario");
-			System.out.println("Login is success");
-		} else {
-			System.out.println("Login is Fail");
-			Assert.fail("Login is unsucessful");
+		@AfterMethod
+		public void quit() {
+	//		rep.endTest(test);
+	// 	rep.flush();
 		}
 	}
-
-	@AfterMethod
-	public void quit() {
-	//	rep.endTest(test);
-	//	rep.flush();
-	}
-
-}
